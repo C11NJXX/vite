286c8774fa60137a55fea5dfeda1068b4c9b945f
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-12 15:14:31 +0800

    Day1(AM) (Using Sass)

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './sass/main.scss'
 
 import Header from './component/Header'
 
